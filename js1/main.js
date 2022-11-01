b8f1707c9a3e38a41d70c643f0cbdec206e4d2c9
arh1tector <novbli33@gmail.com>
2022-11-01 23:09:06 +0200

    js1

---
+++ js1/main.js
@@ -0,0 +1,52 @@
+
+console.log( cell)
+let cell1 =" 'owu'"
+console.log(cell1)
+let cell2 ="'com'"
+console.log(cell2)
+let cell3 = "'ua'"
+console.log(cell3)
+
+let num = 1
+console.log(num)
+let num1 =10
+console.log(num1)
+let num2 =-999
+console.log(num2)
+let num3 =123
+console.log(num3)
+let num4 = 3.14
+console.log(num4)
+let num5 = 2.7
+console.log(num5)
+let num6 =16
+console.log(num6)
+let a = 4<5
+console.log(a)
+let b = 4>5
+console.log(b)
+
+let firstName = 'Denys'
+console.log(firstName)
+let middleName = 'Viktorovich'
+console.log(middleName)
+let lastName= 'Demenkov'
+console.log(lastName)
+let person =firstName +' '+ middleName +' '+ lastName
+console.log(person)
+
+let c = 100
+console.log(typeof c)
+let d = '100'
+console.log(typeof d)
+let r = true
+console.log(typeof r)
+
+
+
+
+
+
+
+
+
